1762368ce750fa1dd15b6cce4ed0f024290c409f
moisesescarcega <moi.escarcega@pm.me>
2025-10-26 15:02:33 -0600

    preparacion para vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,25 @@
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter({
-		pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-	}) }
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter(),
+  }
 };
+// const config = {
+// 	// Consult https://svelte.dev/docs/kit/integrations
+// 	// for more information about preprocessors
+// 	preprocess: vitePreprocess(),
+// 	kit: { adapter: adapter({
+// 		pages: 'build',
+// 			assets: 'build',
+// 			fallback: undefined,
+// 			precompress: false,
+// 			strict: true
+// 	}) }
+// };
 
 export default config;
